7b4466f38be6601f268e2a28b3ed5d479ac2079c
Rohan c <rohanmc9184@gmail.com>
2023-11-01 13:31:07 +0530

    Netlify configure

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -6,8 +6,8 @@ import { WebSocketLink } from '@apollo/client/link/ws'; // Import WebSocketLink
 import { split } from '@apollo/client/link/core';
 import { getMainDefinition } from '@apollo/client/utilities';
 
-const httpUri = 'http://localhost:8000/graphql'; // HTTP URI
-const wsUri = 'ws://localhost:8000/graphql'; // WebSocket URI
+const httpUri = 'https://rochat-be.onrender.com/graphql'; // HTTP URI
+const wsUri = 'ws://rochat-be.onrender.com/graphql'; // WebSocket URI
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   // Create an HTTP link
